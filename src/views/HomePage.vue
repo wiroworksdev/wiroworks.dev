b9a806e255e2f3f3707ba66832b850d8443283aa
Rob Huijben <mail@robhuijben.com>
2024-01-22 21:54:19 +0700

    feat: Add team page to project

---
+++ src/views/HomePage.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <div class="bg-white py-10 sm:py-14">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div
+        class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div class="lg:pr-4">
+          <div
+            class="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
+            <img class="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0"
+              src="https://images.unsplash.com/photo-1630569267625-157f8f9d1a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80"
+              alt="" />
+            <div class="absolute inset-0 bg-gray-900 mix-blend-multiply" />
+            <div class="absolute left-1/2 top-1/2 -ml-16 -translate-x-1/2 -translate-y-1/2 transform-gpu blur-3xl"
+              aria-hidden="true">
+              <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-40"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+            </div>
+            <figure class="relative isolate">
+              <svg viewBox="0 0 162 128" fill="none" aria-hidden="true"
+                class="absolute -left-2 -top-4 -z-10 h-32 stroke-white/20">
+                <path id="0ef284b8-28c2-426e-9442-8655d393522e"
+                  d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z" />
+                <use href="#0ef284b8-28c2-426e-9442-8655d393522e" x="86" />
+              </svg>
+              <blockquote class="mt-6 text-xl font-semibold leading-8 text-white">
+                <p>“We try to support the open source world by filling the gaps in the most used software
+                  systems by building decent add-ons on top of existing hierarchy.”</p>
+              </blockquote>
+              <figcaption class="mt-6 text-sm leading-6 text-gray-300"><strong class="font-semibold text-white">Rob
+                  Huijben & Willem Oostelbos,</strong> CEO's at
+                Wiroworks</figcaption>
+            </figure>
+          </div>
+        </div>
+        <div>
+          <div class="text-base leading-7 text-gray-700">
+            <p class="text-base font-semibold leading-7 text-violet-900">We're getting started</p>
+            <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Software for common and
+              uncommon use cases</h1>
+            <div class="max-w-xl">
+              <p class="mt-6">Our business mission is to develop versatile software solutions for diverse use
+                cases and industries, catering to both common and uncommon needs with equal proficiency. At
+                the core of our philosophy lies a deep understanding that one-size-fits-all approach doesn't
+                work in software development. Every client has unique requirements and faces unique
+                challenges that demand customized solutions. We make it our business to deliver reliable,
+                scalable, and cost-efficient software applications that align perfectly with the specific
+                needs of our clients.</p>
+              <p class="mt-8">We understand that software development is a constantly evolving field where
+                technologies, trends, and practices change frequently. To stand ahead of the innovation
+                curve, we continuously train and equip ourselves with the latest tools, methodologies, and
+                technologies. Our experienced software development teams leverage their domain expertise,
+                creativity, and technical skills to solve complex problems for our clients. From ideation to
+                implementation, we prioritize collaboration, communication, and transparency while
+                delivering software solutions that meet or exceed our clients' expectations.</p>
+
+              <p class="mt-8">
+                At the center of our software development philosophy lies the user. Our software solutions
+                are designed with the end-users in mind, ensuring that the products are easy to use and
+                deliver a rich user experience. We take pride in developing software applications that are
+                intuitive, interactive, and user-friendly. We carry out extensive testing, feedback, and
+                refinement to ensure that the final product is robust, reliable, and stable. Our goal is to
+                make software applications that are not just functional but also enjoyable to use, thus
+                increasing user engagement and satisfaction.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/TheTeam.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="bg-white py-12 md:py-16 lg:py-20">
+        <div class="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our team</h2>
+                <p class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic duo who are passionate
+                    about what we do and dedicated to delivering the best results for our clients.</p>
+            </div>
+            <ul role="list"
+                class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-8 xl:col-span-2">
+                <li v-for="person in people" :key="person.name">
+                    <img class="aspect-[1/1] w-full rounded-2xl object-cover" :src="person.imageUrl" alt="" />
+                    <h3 class="mt-6 text-lg font-semibold leading-8 text-gray-900">{{ person.name }}</h3>
+                    <p class="text-base italic leading-7 text-gray-600">{{ person.role }}</p>
+                    <p class="mt-4 text-base leading-7 text-gray-600">{{ person.bio }}</p>
+                    <ul role="list" class="mt-6 flex gap-x-6">
+                        <li>
+                            <a :href="person.githubUrl" class="text-gray-400 hover:text-gray-500">
+                                <span class="sr-only">Github</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" aria-hidden="true"
+                                    fill="currentColor" viewBox="0 0 496 512">
+                                    <path
+                                        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a :href="person.linkedinUrl" class="text-gray-400 hover:text-gray-500">
+                                <span class="sr-only">LinkedIn</span>
+                                <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+const people = [
+    {
+        name: 'Willem Oostelbos',
+        role: 'PHP Developer',
+        imageUrl:
+            '../src/assets/img/team/willem-oostelbos.png',
+        bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
+        githubUrl: 'https://github.com/woostelbos',
+        linkedinUrl: 'https://www.linkedin.com/in/willem-oostelbos-60247a28/',
+    },
+    {
+        name: 'Rob Huijben',
+        role: 'Frontend Developer',
+        imageUrl:
+            '../src/assets/img/team/rob-huijben.png',
+        bio: 'With over 20 years of experience in web development, I have a broad knowledge of the web development landscape. I have worked with many different technologies, frameworks and CMS systems like Magento, Wordpress and Joomla.',
+        githubUrl: 'https://github.com/robhuijben',
+        linkedinUrl: 'https://www.linkedin.com/in/rhuijben/',
+    }
+]
+</script>
